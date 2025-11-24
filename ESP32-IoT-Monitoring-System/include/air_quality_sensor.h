68eaa06f8c4db0cd8fb8fb886e01e3e3c9644a15
MSI\ADMINS <thanh.nguyenthinh@hcmut.edu.vn>
2025-11-24 18:53:04 +0700

    Add air sensor task

---
+++ ESP32-IoT-Monitoring-System/include/air_quality_sensor.h
@@ -0,0 +1,8 @@
+#ifndef AIR_QUALITY_SENSOR_H
+#define AIR_QUALITY_SENSOR_H
+
+#include "global.h"
+
+void air_quality_task(void * pvParameter);
+
+#endif
---
+++ ESP32-IoT-Monitoring-System/src/air_quality_sensor.cpp
@@ -0,0 +1,31 @@
+#include "air_quality_sensor.h"
+
+void air_quality_task(void * pvParameter){
+    float so2 = 0.0f;
+    float pm10 = 0.0f;
+    float pm25 = 0.0f;
+    char json[64];
+
+    for(;;) {
+        vTaskDelay(200 / portTICK_PERIOD_MS);
+
+        // Wait for trigger to read sensor
+        EventBits_t uxBits = xEventGroupWaitBits(
+        xSensorEventGroup,
+        SENSOR_TRIGGER_READ_ALL_BIT,
+        pdTRUE,
+        pdFALSE,
+        portMAX_DELAY
+        );
+        
+        so2         = (rand() % 100) / 1.0;         // random 0-99
+        pm10        = (rand() % 200) / 1.0;         // random 0-199
+        pm25        = (rand() % 150) / 1.0;         // random 0-149
+
+        snprintf(json, sizeof(json), "{\"so2\":%.2f,\"pm10\":%.2f,\"pm25\":%.2f}", so2, pm10, pm25);
+        if (xQueueSend(xSensorDataQueue, json, 0) != pdPASS) {
+            Serial.println("Error: [AIR] Failed to send telemetry data to queue");
+        }
+        // Serial.printf("Info: [AIR] Done (%dms)\n", millis() - start);
+    }
+}
